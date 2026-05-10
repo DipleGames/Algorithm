41478d1604d872f8bf127875ec8266499e2363f7
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-05-11 01:07:30 +0900

    [level 1] Title: 달리기 경주, Time: 254.46 ms, Memory: 188 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/README.md
+++ 프로그래머스/1/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [level 1] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=csharp) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 349.76 ms
+메모리: 188 MB, 시간: 254.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 19일 20:09:46
+2026년 05월 11일 01:07:24
 
 ### 문제 설명
 
--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.cs
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.cs
@@ -5,25 +5,29 @@ public class Solution
 {
     public string[] solution(string[] players, string[] callings) 
     {
-        Dictionary<string, int> playerIndex = new Dictionary<string, int>();
+        Dictionary<string, int> rank = new Dictionary<string, int>();
+
         for (int i = 0; i < players.Length; i++)
         {
-            playerIndex[players[i]] = i;
+            rank[players[i]] = i;
         }
+
         foreach (string call in callings)
         {
-            int currentIndex = playerIndex[call];
-            if (playerIndex.ContainsKey(call))
-            {
-                string previousPlayer = players[currentIndex - 1];
-                
-                players[currentIndex - 1] = players[currentIndex];
-                players[currentIndex] = previousPlayer;
-                
-                playerIndex[call] = currentIndex - 1;
-                playerIndex[previousPlayer] = currentIndex;
-            }
+            int curIndex = rank[call];
+            int prevIndex = curIndex - 1;
+
+            string prevPlayer = players[prevIndex];
+
+            // 배열에서 위치 교환
+            players[prevIndex] = call;
+            players[curIndex] = prevPlayer;
+
+            // Dictionary 인덱스 갱신
+            rank[call] = prevIndex;
+            rank[prevPlayer] = curIndex;
         }
+
         return players;
     }
 }
