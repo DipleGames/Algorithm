9b26aaaa37ea8672a23c4d0c53d0028b1e1709f3
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-09-24 23:29:25 +0900

    [level 2] Title: 테이블 해시 함수, Time: 9.72 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cs
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cs
@@ -2,30 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Solution
+public class Solution 
 {
-    public int solution(int[,] data, int col, int row_begin, int row_end)
+    public int solution(int[,] data, int col, int row_begin, int row_end) 
     {
         int rowCount = data.GetLength(0);
         int colCount = data.GetLength(1);
         List<int[]> rows = new List<int[]>();
-
-        // 2차원 배열에서 행을 하나씩 꺼내기
-        for (int r = 0; r < rowCount; r++)
+        
+        for(int r=0; r<rowCount; r++)
         {
             int[] row = new int[colCount];
-
-            for (int c = 0; c < colCount; c++)
-                row[c] = data[r, c];
-
+            for(int c=0; c<colCount; c++)
+            {
+                row[c] = data[r,c];
+            }
             rows.Add(row);
         }
-
-        // 정렬만 LINQ 사용
-        rows = rows.OrderBy(row => row[col - 1])
-                   .ThenByDescending(row => row[0])
+        
+        rows = rows.OrderBy(x => x[col - 1])
+                   .ThenByDescending(x => x[0])
                    .ToList();
-
+        
         int answer = 0;
 
         for (int i = row_begin; i <= row_end; i++)
@@ -33,7 +31,9 @@ public class Solution
             int sum = 0;
 
             foreach (int value in rows[i - 1])
+            {
                 sum += value % i;
+            }
 
             answer ^= sum;
         }
